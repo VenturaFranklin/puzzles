06e5e4799277355e9b76f0f0bb934d9be81160cc
Franklin Ventura DEV-Laptop <franklin.ventura@ventana.roche.com>
2015-06-02 15:25:06 -0700

    Working Fails if line 64 is *2 Takes too long at *3 Need to find a faster way

--- src/square_supplies/solution.py
+++ src/square_supplies/solution.py
@@ -3,7 +3,14 @@ Created on Jun 2, 2015
 
 @author: venturf2
 '''
-def answer(n):
+from itertools import chain, combinations
+
+def test():
+    for number in [24,160,9984]:
+        print answer(number)
+
+# @profile
+def answer(totalcoins):
     '''With the zombie cure injections ready to go,
     it's time to start treating our zombified rabbit friends (known as zombits)
     at our makeshift zombit treatment center. You need to run out really fast to buy
@@ -42,5 +49,22 @@ def answer(n):
     Output:
         (int) 2
     '''
-    
-    pass
+    cost=0
+    x = 1
+    costs=[]
+    while cost < totalcoins:
+        x += 1
+        cost = x**2
+        if cost < totalcoins:
+            costs.append(cost)
+    for combo in powerset(costs):
+#         print combo
+        if sum(combo) == totalcoins:
+            return len(combo)
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)*3
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+test()
