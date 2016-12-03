710c28a061837662c9ccdf2e035f1f93ae1470d2
Franklin Ventura DEV-Laptop <franklin.ventura@ventana.roche.com>
2016-12-02 23:02:57 -0700

    Second part of Day 3

--- src/Advent16/Day3/SquaresWithThreeSides.py
+++ src/Advent16/Day3/SquaresWithThreeSides.py
@@ -21,7 +21,27 @@ than the remaining side. For example, the "triangle" given above is impossible,
 because 5 + 10 is not larger than 25.
 
 In your puzzle input, how many of the listed triangles are possible?
+
+--- Part Two ---
+
+Now that you've helpfully marked up their design documents,
+it occurs to you that triangles are specified in groups of three vertically.
+Each set of three numbers in a column specifies a triangle. Rows are unrelated.
+
+For example, given the following specification,
+numbers with the same hundreds digit would be part of the same triangle:
+
+101 301 501
+102 302 502
+103 303 503
+201 401 601
+202 402 602
+203 403 603
+In your puzzle input, and instead reading by columns,
+how many of the listed triangles are possible?
 '''
+from numpy import array
+import numpy as np
 
 
 def test1_run():
@@ -32,7 +52,6 @@ def test1_run():
 
 
 def run(sides):
-    sides = map(int, sides)
     sides.sort()
     l1, l2, l3 = sides
     if (l1 > l2+l3) or (l2 > l1+l3) or (l3 > l1+l2):
@@ -43,8 +62,7 @@ def run(sides):
         return True
 
 if __name__ == "__main__":
-#     test1_run()
-    count = 0
+    sides = array([0, 0, 0])
     with open('PuzzleInput.txt', 'r') as this_file:
         for side in this_file:
             side = side.replace('\n', '')
@@ -53,7 +71,19 @@ if __name__ == "__main__":
             side = map(str.strip, side)
             if '' in side:
                 side.remove('')
-            print(side)
-            if run(side):
-                count += 1
+            side = map(int, side)
+            side = array([side])
+            sides = np.vstack((sides, side))
+    sides = np.delete(sides, 0, 0)
+    num = 0
+    new_sides = []
+    while num < len(sides):
+        new_side = zip(sides[num], sides[num+1], sides[num+2])
+        new_side = map(list, new_side)
+        new_sides.extend(new_side)
+        num += 3
+    count = 0
+    for side in new_sides:
+        if run(side):
+            count += 1
     print(count)
