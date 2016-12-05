dce74139074e5b0ab3f2e60d78db93eb2db5983d
Franklin Ventura DEV-Laptop <franklin.ventura@ventana.roche.com>
2016-12-04 20:34:11 -0700

    Day 4 part 2

--- src/Advent16/Day4/SecurityThroughObscurity.py
+++ src/Advent16/Day4/SecurityThroughObscurity.py
@@ -26,8 +26,28 @@ totally-real-room-200[decoy] is not.
 Of the real rooms from the list above, the sum of their sector IDs is 1514.
 
 What is the sum of the sector IDs of the real rooms?
+
+--- Part Two ---
+
+With all the decoy data out of the way,
+it's time to decrypt this list and get moving.
+
+The room names are encrypted by a state-of-the-art shift cipher,
+which is nearly unbreakable without the right software.
+However, the information kiosk designers at Easter Bunny HQ were not expecting
+to deal with a master cryptographer like yourself.
+
+To decrypt a room name, rotate each letter forward through the alphabet a
+number of times equal to the room's sector ID.
+A becomes B, B becomes C, Z becomes A, and so on.
+Dashes become spaces.
+
+For example, the real name for qzmt-zixmtkozy-ivhz-343 is very encrypted name.
+
+What is the sector ID of the room where North Pole objects are stored?
 '''
 from collections import Counter
+import string
 
 
 def test1_run():
@@ -45,6 +65,14 @@ def test1_run():
     assert expected_out_final == actual_out_final
 
 
+def test_shift():
+    name = 'qzmt zixmtkozy ivhz'
+    shift = 343
+    actual_out = shift_name(name, shift)
+    expected_out = 'very encrypted name'
+    assert expected_out == actual_out
+
+
 def reverse_dict(dict_old):
     dict_new = {}
     for key, value in dict_old.items():
@@ -55,12 +83,26 @@ def reverse_dict(dict_old):
     return dict_new
 
 
+def shift_name(name, shift):
+    letters = list(string.ascii_lowercase)
+    new_name = ''
+    for letter in name:
+        if letter == ' ':
+            new_letter = letter
+        else:
+            indx = letters.index(letter)
+            new_letter = letters[(shift + indx) % len(letters)]
+        new_name += new_letter
+    return new_name
+
+
 def run(room):
     sector_id = room.split('-')[-1]
     sector_id, check = sector_id.split('[')
     check = check[:-1]
-    name = room.split('-')[:-1]
-    name = ''.join(name)
+    name_raw = room.split('-')[:-1]
+    name = ''.join(name_raw)
+    name_raw = ' '.join(name_raw)
     values = Counter(name)
     check_test = ''
     dict_new = reverse_dict(values)
@@ -69,17 +111,18 @@ def run(room):
         letters = dict_new[count]
         check_test += ''.join(sorted(letters))
     if check_test.split(check)[0] == '':
-        return True, int(sector_id)
+        return True, int(sector_id), name_raw
     else:
-        return False, False
+        return False, False, False
 
 
 if __name__ == "__main__":
-    out = 0
+    test_shift()
     with open('PuzzleInput.txt', 'r') as this_file:
         for room in this_file:
             room = room.replace('\n', '')
-            test, sector_id = run(room)
+            test, sector_id, name_raw = run(room)
             if test:
-                out += sector_id
-    print(out)
+                new_name = shift_name(name_raw, sector_id)
+                if 'north' in new_name:
+                    print(new_name, sector_id)
