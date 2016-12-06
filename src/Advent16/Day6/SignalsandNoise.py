6df287c4948537e2f2a13c4bc0e7ccd7da466e3d
Franklin Ventura DEV-Laptop <franklin.ventura@ventana.roche.com>
2016-12-06 16:48:45 -0700

    Day 6 part 2 complete

--- src/Advent16/Day6/SignalsandNoise.py
+++ src/Advent16/Day6/SignalsandNoise.py
@@ -39,6 +39,25 @@ error-corrected message, easter.
 
 Given the recording in your puzzle input, what is the error-corrected
 version of the message being sent?
+
+--- Part Two ---
+
+Of course, that would be the message - if you hadn't agreed to use a modified
+repetition code instead.
+
+In this modified code, the sender instead transmits what looks like random
+data, but for each character, the character they actually want to send is
+slightly less likely than the others. Even after signal-jamming noise,
+you can look at the letter distributions in each column and choose the least
+common letter to reconstruct the original message.
+
+In the above example, the least common character in the first column is a;
+in the second, d, and so on. Repeating this process for the remaining
+characters produces the original message, advent.
+
+Given the recording in your puzzle input and this new decoding methodology,
+what is the original message that Santa is trying to send?
+
 '''
 from collections import Counter
 
@@ -65,6 +84,28 @@ enarar'''
     assert actual_out == expected_out
 
 
+def test_2():
+    message = '''eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar'''
+    actual_out = run(message)
+    expected_out = 'advent'
+    assert actual_out == expected_out
+
+
 def run(message):
     message_split = message.split('\n')
     chars = {}
@@ -77,14 +118,14 @@ def run(message):
                 chars[i] = [char]
     out_message = [0] * line_len
     for char in chars:
-        letter = Counter(chars[char]).most_common(1)[0][0]
+        letter = Counter(chars[char]).most_common()[-1][0]
         out_message[char] = letter
     out = ''.join(out_message)
     return out
 
 
 if __name__ == "__main__":
-#     test_1()
+#     test_2()
     with open('PuzzleInput.txt', 'r') as this_file:
         out = run(this_file.read())
     print(out)
