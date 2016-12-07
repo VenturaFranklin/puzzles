c1c2e57fc5aa47412dec25f98da02d8d800efa13
Franklin Ventura DEV-Laptop <franklin.ventura@ventana.roche.com>
2016-12-07 10:43:33 -0700

    Part 2 Setup & terminology update supernet

--- src/Advent16/Day7/InternetProtocolVersion7.py
+++ src/Advent16/Day7/InternetProtocolVersion7.py
@@ -26,6 +26,32 @@ aaaa[qwer]tyui does not support TLS
 ioxxoj[asdfgh]zxcvbn supports TLS
 (oxxo is outside square brackets, even though it's within a larger string).
 How many IPs in your puzzle input support TLS?
+
+--- Part Two ---
+
+You would also like to know which IPs support SSL (super-secret listening).
+
+An IP supports SSL if it has an Area-Broadcast Accessor, or ABA,
+anywhere in the supernet sequences (outside any square bracketed sections),
+and a corresponding Byte Allocation Block, or BAB,
+anywhere in the hypernet sequences.
+An ABA is any three-character sequence which consists of the same character
+twice with a different character between them, such as xyx or aba.
+A corresponding BAB is the same characters but in reversed positions:
+yxy and bab, respectively.
+
+For example:
+
+aba[bab]xyz supports SSL (aba outside square brackets with corresponding
+bab within square brackets).
+xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
+aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet;
+the aaa sequence is not related,
+because the interior character must be different).
+zazbz[bzb]cdb supports SSL (zaz has no corresponding aza,
+but zbz has a corresponding bzb, even though zaz and zbz overlap).
+
+How many IPs in your puzzle input support SSL?
 '''
 
 
@@ -43,14 +69,14 @@ def test_1():
 def split_ip(test_ip):
     splits = test_ip.split('[')
     all_splits = {'hypernet': [],
-                  'outside': []}
+                  'supernet': []}
     for spl in splits:
         if ']' in spl:
             this_split = spl.split(']')
             all_splits['hypernet'].append(this_split[0])
-            all_splits['outside'].append(this_split[1])
+            all_splits['supernet'].append(this_split[1])
         else:
-            all_splits['outside'].append(spl)
+            all_splits['supernet'].append(spl)
     return all_splits
 
 
@@ -71,7 +97,7 @@ def subgroups(this_string):
 def run(test_ip):
     splits = split_ip(test_ip)
     TLS = False
-    for test_split in splits['outside']:
+    for test_split in splits['supernet']:
         for test in subgroups(test_split):
             if abba(test):
                 TLS = True
