8efe2079989e54736c216ba2431b706329d0d6d8
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2017-12-02 07:38:08 -0700

    Day 2 Part 2 Start

--- src/Advent17/Day02/CorruptionChecksum.py
+++ src/Advent17/Day02/CorruptionChecksum.py
@@ -29,11 +29,40 @@ The third row's difference is 6.
 In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
 
 What is the checksum for the spreadsheet in your puzzle input?
+
+--- Part Two ---
+
+"Great work; looks like we're on the right track after all.
+Here's a star for your effort." However, the program seems a little worried.
+Can programs be worried?
+
+"Based on what we're seeing, it looks like all the User wanted
+is some information about the evenly divisible values in the spreadsheet.
+Unfortunately, none of us are equipped for that kind of calculation -
+most of us specialize in bitwise operations."
+
+It sounds like the goal is to find the only two numbers in each row where one
+evenly divides the other - that is, where the result of the division operation
+is a whole number. They would like you to find those numbers on each line,
+divide them, and add up each line's result.
+
+For example, given the following spreadsheet:
+
+5 9 2 8
+9 4 7 3
+3 8 6 5
+In the first row, the only two numbers that evenly divide are 8 and 2;
+the result of this division is 4.
+In the second row, the two numbers are 9 and 3; the result is 3.
+In the third row, the result is 2.
+In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+What is the sum of each row's result in your puzzle input?
 '''
 
 
 def test_p10():
-    testing	=	'''5	1	9	5\n7	5	3\n2	4	6	8'''
+    testing = '''5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8'''
     actual_out = run(testing)
     expected_out = 18
     assert actual_out == expected_out, "{} != {}".format(actual_out,
@@ -54,9 +83,9 @@ def run(test):
 
 
 def test_p20():
-    testing = ''''''
+    testing = '''5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5'''
     actual_out = run2(testing)
-    expected_out = 6
+    expected_out = 9
     assert actual_out == expected_out, "{} != {}".format(actual_out,
                                                          expected_out)
 
