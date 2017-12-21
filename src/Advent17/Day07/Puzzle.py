244170b3d009c7aeecb15e49e29bee5adbc39020
Franklin Ventura DEV-Laptop <venturafranklin@gmail.com>
2017-12-21 13:51:24 -0700

    Day 07 part 2 start

--- src/Advent17/Day07/Puzzle.py
+++ src/Advent17/Day07/Puzzle.py
@@ -72,6 +72,41 @@ and are all the tops of their own towers.
 Before you're ready to help them,
 you need to make sure your information is correct.
 What is the name of the bottom program?
+
+--- Part Two ---
+The programs explain the situation: they can't get down.
+Rather, they could get down,
+if they weren't expending all of their energy trying to
+keep the tower balanced. Apparently, one program has the wrong weight,
+and until it's fixed, they're stuck here.
+
+For any program holding a disc,
+each program standing on that disc forms a sub-tower.
+Each of those sub-towers are supposed to be the same weight,
+or the disc itself isn't balanced.
+The weight of a tower is the sum of the weights of the programs in that tower.
+
+In the example above, this means that for ugml's disc to be balanced,
+gyxo, ebii, and jptl must all have the same weight, and they do: 61.
+
+However, for tknk to be balanced,
+each of the programs standing on its disc and all
+programs above it must each match.
+This means that the following sums must all be the same:
+
+ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
+As you can see, tknk's disc is unbalanced: ugml's
+stack is heavier than the other two.
+Even though the nodes above ugml are balanced,
+ugml itself is too heavy:
+it needs to be 8 units lighter for its stack to weigh
+243 and keep the towers balanced.
+If this change were made, its weight would be 60.
+
+Given that exactly one program is the wrong weight,
+what would its weight need to be to balance the entire tower?
 '''
 
 
@@ -111,12 +146,24 @@ def run(test):
     return list(set(supporting_programs) - set(subprogram_names))[0]
 
 
-# def test_p20():
-#     testing = '''5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5'''
-#     actual_out = run2(testing)
-#     expected_out = 9
-#     assert actual_out == expected_out, "{} != {}".format(actual_out,
-#                                                          expected_out)
+def test_p20():
+    testing = '''pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)'''
+    actual_out = run2(testing)
+    expected_out = 60
+    assert actual_out == expected_out, "{} != {}".format(actual_out,
+                                                         expected_out)
 
 
 def run2(test):
