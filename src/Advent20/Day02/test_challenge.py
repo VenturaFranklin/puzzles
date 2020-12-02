11659f1da89637312e0a7ab243f5ba1cdf048f97
Franklin Ventura <venturafranklin@gmail.com>
2020-12-02 16:41:40 -0700

    2020 Day2 Setup

---
+++ src/Advent20/Day02/test_challenge.py
@@ -0,0 +1,19 @@
+from .challenge import run, run2
+
+
+def test1_1_run():
+    instructions = [
+        "1-3 a: abcde",
+        "1-3 b: cdefg",
+        "2-9 c: ccccccccc",
+    ]
+    expected_out = 2
+    actual_out = run(instructions)
+    assert expected_out == actual_out
+
+
+def test1_1_run2():
+    instructions = []
+    expected_out = 0
+    actual_out = run2(instructions)
+    assert expected_out == actual_out
